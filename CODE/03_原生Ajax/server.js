4caec05fa5b82d4c67167be97f3304490593d2e3
yiwen0313 <2469119402@qq.com>
2021-09-18 16:26:21 +0800

    AJAX设置请求头信息 学习完成

--- CODE/03_原生Ajax/server.js
+++ CODE/03_原生Ajax/server.js
@@ -15,9 +15,14 @@ app.get('/server', (request, response)=>{
     response.send('HELLO  AJAX');
 });
 
-app.post('/server', (request, response)=>{
+// app.post('/server', (request, response)=>{
+// 可以接收任意类型的请求
+app.all('/server', (request, response)=>{
     // 设置响应头  设置允许跨越
     response.setHeader('Access-Control-Allow-Origin', '*');
+
+    //响应头
+    response.setHeader('Access-Control-Allow-Headers', '*');
 
     // 设置响应
     response.send('HELLO AJAX POST');
