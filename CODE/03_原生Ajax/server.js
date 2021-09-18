ee7778fab211467f11656bb929226603194a8dc2
yiwen0313 <2469119402@qq.com>
2021-09-18 19:32:55 +0800

    请求超时与网络异常处理 学习完成

--- CODE/03_原生Ajax/server.js
+++ CODE/03_原生Ajax/server.js
@@ -7,26 +7,34 @@ const app = express();
 // 3.创建路由规则
 // request 是对请求报文的封装
 // response 是对响应报文的封装
-app.get('/server', (request, response)=>{
+app.get('/server', (request, response) => {
     // 设置响应头  设置允许跨越
     response.setHeader('Access-Control-Allow-Origin', '*');
-
     // 设置响应
     response.send('HELLO  AJAX');
 });
 
 // 针对IE缓存
-app.get('/ie', (request, response)=>{
+app.get('/ie', (request, response) => {
     // 设置响应头  设置允许跨越
     response.setHeader('Access-Control-Allow-Origin', '*');
-
     // 设置响应
-    response.send('HELLO IE11333');
+    response.send('HELLO IE');
+});
+
+// 延时响应
+app.get('/delay', (request, response) => {
+    // 设置响应头  设置允许跨越
+    response.setHeader('Access-Control-Allow-Origin', '*');
+    setTimeout(() => {
+        // 设置响应
+        response.send('延时响应');
+    }, 3000);
 });
 
 // app.post('/server', (request, response)=>{
 // 可以接收任意类型的请求
-app.all('/json-server', (request, response)=>{
+app.all('/json-server', (request, response) => {
     // 设置响应头  设置允许跨越
     response.setHeader('Access-Control-Allow-Origin', '*');
 
@@ -44,6 +52,6 @@ app.all('/json-server', (request, response)=>{
 });
 
 // 4.监听端口 启动服务
-app.listen(8000, ()=>{
+app.listen(8000, () => {
     console.log("服务已经启动, 8000端口监听中...");
 });
