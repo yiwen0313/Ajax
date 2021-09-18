4b4c51c4433f4212e3a0efa07dac8190aa418f2f
yiwen0313 <2469119402@qq.com>
2021-09-18 17:32:00 +0800

    服务端响应JSON数据 学习完成

--- CODE/03_原生Ajax/server.js
+++ CODE/03_原生Ajax/server.js
@@ -17,15 +17,21 @@ app.get('/server', (request, response)=>{
 
 // app.post('/server', (request, response)=>{
 // 可以接收任意类型的请求
-app.all('/server', (request, response)=>{
+app.all('/json-server', (request, response)=>{
     // 设置响应头  设置允许跨越
     response.setHeader('Access-Control-Allow-Origin', '*');
 
     //响应头
     response.setHeader('Access-Control-Allow-Headers', '*');
 
+    // 响应一个数据
+    const data = {
+        name: 'gxy'
+    };
+    // 对对象进行字符串的转换
+    let str = JSON.stringify(data);
     // 设置响应
-    response.send('HELLO AJAX POST');
+    response.send(str);
 });
 
 // 4.监听端口 启动服务
